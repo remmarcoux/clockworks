773c208b641b98f980edf4949334b0c24d009c8f
François Drouin-Morin <francois.drouinm@gmail.com>
2020-02-15 15:17:04 -0500

    Integrated Winston and Config

---
+++ loggingoptions.js
@@ -0,0 +1,9 @@
+const winston = require("winston");
+
+module.exports = {
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console()
+    ]
+};
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const config = require("config");
+const winston = require("winston");
+const expressWinston = require("express-winston");
 let app = express();
+
+const logger = winston.createLogger(require("./loggingoptions.js"));
+
+app.use(expressWinston.logger(logger));
 
 app.get("/", function(req, res){
     res.send("hello world");
 });
 
-app.listen(8081, function(){
-    console.log("Application listening on port 8081")
+app.use(expressWinston.errorLogger(logger));
+
+app.listen(config.get("server.port"), function(){
+    logger.info(`Application listening on port ${config.get("server.port")}`)
 });
