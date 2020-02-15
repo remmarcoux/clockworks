00084a19d8e480f441657e9d8339493a55d9cee0
François Drouin-Morin <francois.drouinm@gmail.com>
2020-02-15 18:53:06 -0500

    Added app hosting

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const winston = require("winston");
 const expressWinston = require("express-winston");
 const mongoose = require("mongoose");
 let app = express();
+const path = require("path");
 
 mongoose.connect(config.get("database.connectionString"),  { useNewUrlParser: true });
 let db = mongoose.connection;
@@ -12,13 +13,14 @@ const logger = winston.createLogger(require("./loggingoptions.js"));
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'ClientApp/build')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ClientApp/build', 'index.html'));
+});
+
 app.use(expressWinston.logger(logger));
 
 app.use(require("./api/clock.router.js"));
-
-app.get("/", function(req, res){
-    res.send("hello world");
-});
 
 app.use(expressWinston.errorLogger(logger));
 
