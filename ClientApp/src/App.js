eedcecbebe2f4a7833cefb108a0e9af3f62bf594
ReM <remmarcoux@gmail.com>
2020-02-15 19:04:44 -0500

    Data feed overhaul

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,26 +1,96 @@
 import React from 'react';
-import Clock from './clocks/clock';
+import ClockControls from './clocks/clockControls';
 import './App.css';
 
-const CLOCK_LAST_ID = "lastClockId";
+class App extends React.Component {
+  polling = null;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>ClockWorks</h1>
-      </header>
-      <main>
+  render() {
+    var clocks = [];
+    if(this.state && this.state.clocks)
+    {
+      this.state.clocks.forEach(clock => {
+        clocks.push(<ClockControls clockInfos={ clock }
+                                   onNameChanged={ this.onNameChanged }
+                                   onValueChanged={ this.onValueChanged } />);
+      });
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>ClockWorks</h1>
+        </header>
+        <main>
+          { clocks }
+        </main>
+        <footer>
+        </footer>
+      </div>
+    );  
+  }
 
-        <Clock id="0" clockSize={ 4 } />
-      </main>
-      <footer>
-        {/* <button>Add 4 Clock</button>
-        <button>Add 6 Clock</button>
-        <button>Add 8 Clock</button> */}
-      </footer>
-    </div>
-  );
+  componentDidMount() {
+    this.fetchClocksInfos();
+    this.polling = setInterval(this.fetchClocksInfos, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.polling);
+  }
+
+  setClocksInfos = (data) => {
+    this.setState({
+      clocks: data
+    });
+  }
+
+  // Event Listeners
+  changeField = (id, field, data) => {
+    var clocks = this.state.clocks;
+    var clockIndex = clocks.findIndex((clock)=>clock._id == id);
+    var updatedClock = clocks[clockIndex];
+    
+    updatedClock[field] = data;
+    updatedClock._id = undefined;
+    clocks[clockIndex] = updatedClock;
+
+    this.setState({
+      clocks
+    }, ()=>{ this.putClockById(id, updatedClock);
+             this.fetchClocksInfos(); } );
+  }
+
+  onNameChanged = (id, name) =>{
+    this.changeField(id, "name", name);
+  }
+
+  onValueChanged= (id, value) => {
+    this.changeField(id, "value", value);
+  }
+
+  // Connexion Stuff
+  fetchClocksInfos = () => {
+    fetch(`/api/v1/clock/list?t=${Date.now()}`).then(this.onDataRecieved)
+                                               .then(this.setClocksInfos);
+  }
+
+  putClockById = (clockId, clockInfo) => {
+    fetch(`/api/v1/clock/${clockId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(clockInfo)
+    });
+  }
+
+  onDataRecieved = (res) => {
+    if(res.ok){ 
+      return res.json();
+    } else { 
+      console.log("To Be Implemented");
+    } 
+  }
 }
 
 export default App;
