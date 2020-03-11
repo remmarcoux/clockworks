fa4542b73923eeb34e1a0833997a41771ae6e90a
Renaud Marcoux <remmarcoux@gmail.com>
2020-03-11 10:54:29 -0400

    Clockmaster visual improvements

--- ClientApp/src/clocks/clockMaster.jsx
+++ ClientApp/src/clocks/clockMaster.jsx
@@ -3,42 +3,58 @@ import React from 'react';
 /// Props
 /// - onAddClock
 class ClockMaster extends React.Component {
-    defaultClockState = {
+    defaultClockAddState = {
         clockName: "",
         segmentsCount: 4,
     }
 
-    state = this.defaultClockState
+    state = this.defaultClockAddState
 
     render() {
         return (
-            <div className="clocks-master-controls clock-container">
+            <div className={ "clocks-master-controls clock-container" + (this.state.opened ? " opened" : "") }>
                 <div className="clock-option-pannel">
+                    <div className="clock-option-wrapper">
+                        <div className="clock-master-option">
+                            <label htmlFor="clockName">Clock Name</label>
+                            <input type="text" 
+                                disabled={ !this.state.opened }
+                                name="clockName" 
+                                id="new-clock-name"
+                                value={ this.state.clockName }
+                                onChange={ this.onClockNameChanged } />
+                        </div>
+                        <div className="clock-master-option">
+                            <label htmlFor="segmentsCount"># Segments</label>
+                            <input type="number"
+                                disabled={ !this.state.opened }
+                                name="segmentsCount" 
+                                id="new-clock-segments-count" 
+                                value={ this.state.segmentsCount }
+                                onChange={ this.onClockSegmentsCountChanged }
+                                onBlur={ this.onClockSegmentsCountBlurred }  />
+                        </div>
 
-                    <div className="clock-master-option">
-                        <label htmlFor="clockName">Clock Name</label>
-                        <input type="text" 
-                            name="clockName" 
-                            id="new-clock-name"
-                            value={ this.state.clockName }
-                            onChange={ this.onClockNameChanged } />
-                    </div>
-                    <div className="clock-master-option">
-                        <label htmlFor="segmentsCount"># Segments</label>
-                        <input type="number"
-                            name="segmentsCount" 
-                            id="new-clock-segments-count" 
-                            value={this.state.segmentsCount}
-                            onChange={ this.onClockSegmentsCountChanged }
-                            onBlur={ this.onClockSegmentsCountBlurred }  />
-                    </div>
+                        <button className="clock-add"
+                                disabled={ !this.state.opened } 
+                                onClick={ this.toggleClockAdd } >Cancel</button>
 
-                    <button className="clock-add"
-                            disabled={ this.state.clockName === "" } 
-                            onClick={ this.onAddClock } >Add Clock</button>
+                        <button className="clock-add"
+                                disabled={ !this.state.opened || this.state.clockName === "" } 
+                                onClick={ this.onAddClock } >Add Clock</button>
+                    </div>
+                    <button className="clock-option-toggle-add-menu"
+                            disabled={ this.state.opened }
+                            onClick={ this.toggleClockAdd }>
+                        <i className="fas fa-plus-square"></i>
+                    </button>
                 </div>
             </div>
         );
+    }
+
+    toggleClockAdd = (e) => {
+        this.setState({opened: !this.state.opened});
     }
 
     onClockNameChanged = (e) => {
@@ -72,11 +88,11 @@ class ClockMaster extends React.Component {
         var segmentsCount = this.state.segmentsCount; 
         if( segmentsCount === "")
         {
-            segmentsCount = this.defaultClockState.segmentsCount;
+            segmentsCount = this.defaultClockAddState.segmentsCount;
         }
         
         this.props.onAddClock(this.state.clockName, segmentsCount);
-        this.setState(this.defaultClockState);
+        this.setState(this.defaultClockAddState);
     }
 }
 
